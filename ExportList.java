57742ca7540836f0f1cbe759148e1c08fd3eb3b8
LucidThought <alata.it@gmail.com>
2015-10-07 17:21:24 -0600

    ExportList.java

---
+++ ExportList.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class ExportList
+{
+	public void writeList(MovieNode head)
+	{
+		try
+		{
+			String filename = "movie_list.txt";
+			FileWriter fw = new FileWriter(filename);
+			PrintWriter pw = new PrintWriter(fw);
+	
+			while (head != null)
+			{
+				pw.print(head.getData().printAll());
+				head = head.getNext();
+			}
+			fw.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
